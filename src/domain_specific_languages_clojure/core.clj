9f7ded94b177e774bd1ef226084b1be1090d2e75
Eric Normand <ericwnormand@gmail.com>
2018-03-02 10:52:57 -0600

    Add EscapedString and paraphernalia

--- src/domain_specific_languages_clojure/core.clj
+++ src/domain_specific_languages_clojure/core.clj
@@ -1,42 +1,80 @@
 (ns domain-specific-languages-clojure.core
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str])
+  (:import [org.apache.commons.text StringEscapeUtils]))
+
+(deftype EscapedString [s]
+  Object
+  (toString [this]
+    s)
+  (equals [this other]
+    (if (instance? EscapedString other)
+      (= (.-s this) (.-s other))
+      false)))
+
+(defn unescaped [s]
+  (cond
+    (instance? EscapedString s)
+    s
+
+    (string? s)
+    (EscapedString. s)
+
+    :else
+    (throw (ex-info "I don't know how to unescaped this value." {:value s}))))
+
+(defn escape [s]
+  (cond
+    (instance? EscapedString s)
+    s
+
+    (string? s)
+    (unescaped (StringEscapeUtils/escapeHtml4 s))
+
+    :else
+    (throw (ex-info "I don't know how to escape this value." {:value s}))))
 
 (defn normalize [hiccup]
   (let [[tag attr? & children] hiccup
-          attr (if (map? attr?)
-                 attr?
-                 {})
-          children (if (map? attr?)
-                     children
-                     (cons attr? children))]
+        attr (if (map? attr?)
+               attr?
+               {})
+        children (if (map? attr?)
+                   children
+                   (cons attr? children))]
     (into [tag attr] children)))
 
-;; hiccup -> String (HTML)
+;; hiccup -> EscapedString (HTML)
 (defn eval-hiccup [hiccup]
   (cond
     (nil? hiccup)
-    ""
-    
+    (unescaped "")
+
     (string? hiccup)
-    hiccup
-    
+    (unescaped (escape hiccup))
+
     (number? hiccup)
-    (str hiccup)
-    
+    (unescaped (str hiccup))
+
     (vector? hiccup)
     (let [[tag attr & children] (normalize hiccup)]
-      (str "<" (name tag)
-        (str/join
-          (for [[k v] attr]
-            (str " " (name k) "=\"" v \")))
-        ">"
+      (unescaped
+        (str "<" (name tag)
+          (str/join
+            (for [[k v] attr]
+              (str " " (name k) "=\"" v \")))
+          ">"
 
-        (str/join (map eval-hiccup children))
+          (str/join (map eval-hiccup children))
 
-        "</" (name tag) ">"))
+          "</" (name tag) ">")))
 
     (seq? hiccup)
     (str/join (map eval-hiccup hiccup))
-    
+
     :else
     (throw (ex-info "I don't know how to handle this data as hiccup." {:hiccup hiccup}))))
+
+(defn comment-component [unsanitized-text]
+  (eval-hiccup
+    [:div {:class "comment"}
+     unsanitized-text]))
--- test/domain_specific_languages_clojure/core_test.clj
+++ test/domain_specific_languages_clojure/core_test.clj
@@ -3,37 +3,43 @@
             [clojure.test :as t :refer :all]))
 
 (deftest nil-empty-string
-  (is (= "" (sut/eval-hiccup nil))))
+  (is (= (sut/unescaped "") (sut/eval-hiccup nil))))
 
 (deftest string-itself
-  (is (= "Hello, World!" (sut/eval-hiccup "Hello, World!"))))
+  (is (= (sut/unescaped "Hello, World!") (sut/eval-hiccup "Hello, World!"))))
+
+(deftest string-itself-html
+  (is (= (sut/unescaped "Hello, World! &amp;nbsp;") (sut/eval-hiccup "Hello, World! &nbsp;"))))
 
 (deftest number-string
-  (is (= "123" (sut/eval-hiccup 123))))
+  (is (= (sut/unescaped "123") (sut/eval-hiccup 123))))
 
 (deftest vector-div-html
-  (is (= "<div></div>" (sut/eval-hiccup [:div]))))
+  (is (= (sut/unescaped "<div></div>") (sut/eval-hiccup [:div]))))
 
 (deftest vector-span-html
-  (is (= "<span></span>" (sut/eval-hiccup [:span]))))
+  (is (= (sut/unescaped "<span></span>") (sut/eval-hiccup [:span]))))
 
 (deftest vector-attr-html
-  (is (= "<div class=\"abc\"></div>" (sut/eval-hiccup [:div {:class "abc"}]))))
+  (is (= (sut/unescaped "<div class=\"abc\"></div>") (sut/eval-hiccup [:div {:class "abc"}]))))
 
 (deftest vector-children-html
-  (is (= "<div>abc</div>" (sut/eval-hiccup [:div {} "abc"]))))
+  (is (= (sut/unescaped "<div>abc</div>") (sut/eval-hiccup [:div {} "abc"]))))
 
 (deftest vector-children-no-attr-html
-  (is (= "<div>abc</div>" (sut/eval-hiccup [:div "abc"]))))
+  (is (= (sut/unescaped "<div>abc</div>") (sut/eval-hiccup [:div "abc"]))))
 
 (deftest vector-children-2-children-html
-  (is (= "<div>abcdef</div>" (sut/eval-hiccup [:div "abc" "def"]))))
+  (is (= (sut/unescaped "<div>abcdef</div>") (sut/eval-hiccup [:div "abc" "def"]))))
 
 (deftest vector-div-div-html
-  (is (= "<div><div></div></div>" (sut/eval-hiccup [:div [:div]]))))
+  (is (= (sut/unescaped "<div><div></div></div>") (sut/eval-hiccup [:div [:div]]))))
+
+(deftest vector-div-div-br-escape-html
+  (is (= (sut/unescaped "<div><div>&lt;br&gt;</div></div>") (sut/eval-hiccup [:div [:div "<br>"]]))))
 
 (deftest vector-list-html
-  (is (= "<div><div></div>hello<span>abc</span></div>" (sut/eval-hiccup [:div (list [:div] "hello" [:span "abc"])]))))
+  (is (= (sut/unescaped "<div><div></div>hello<span>abc</span></div>") (sut/eval-hiccup [:div (list [:div] "hello" [:span "abc"])]))))
 
 (deftest unknown-error
   (is (thrown? clojure.lang.ExceptionInfo
