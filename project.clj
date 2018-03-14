e7d2a2fd06ed15f8dd38e2a715270e6036996ca5
Eric Normand <ericwnormand@gmail.com>
2018-03-14 13:08:17 -0500

    Add test-refresh and humane output

--- project.clj
+++ project.clj
@@ -4,4 +4,8 @@
   :license {:name "CC0 1.0 Universal (CC0 1.0) Public Domain Dedication"
             :url "http://creativecommons.org/publicdomain/zero/1.0/"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.apache.commons/commons-text "1.2"]])
+                 [org.apache.commons/commons-text "1.2"]]
+  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.22.0"]]
+                   :dependencies [[pjstadig/humane-test-output "0.8.3"]]
+                   :injections [(require 'pjstadig.humane-test-output)
+                                (pjstadig.humane-test-output/activate!)]}})
