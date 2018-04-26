07f1111777f0c680ac24870ae2190bc295e9efa5
Eric Normand <ericwnormand@gmail.com>
2018-04-25 22:40:25 -0500

    Create Crisp compiler

--- src/domain_specific_languages_clojure/crisp.clj
+++ src/domain_specific_languages_clojure/crisp.clj
@@ -64,3 +64,88 @@
     :else
     (throw (ex-info "I don't know how to evaluate this expression."
              {:expression expr}))))
+
+(defn crisp-compile* [expr]
+  (cond
+    (self-evaling? expr)
+    (fn [env]
+      expr)
+
+    (symbol? expr)
+    (fn [env]
+      (if (contains? env expr)
+        (get env expr)
+        (throw (ex-info "I could not find the symbol in the environment."
+                 {:symbol expr
+                  :environment env}))))
+
+    (seq? expr)
+    (let [[op & args] expr]
+      (case op
+        if (let [[testf thenf elsef]
+                 (map crisp-compile* args)]
+             (fn [env]
+               (if (testf env)
+                 (thenf env)
+                 (elsef env))))
+        quote (let [[quote-expr] args]
+                (fn [env]
+                  quote-expr))
+        do (let [bodyfs (map crisp-compile* args)]
+             (fn [env]
+               (last (map #(% env) bodyfs))))
+        let (let [[binding & body] args]
+              (cond
+                (empty? binding)
+                (crisp-compile* (cons 'do body))
+
+                (= 1 (count binding))
+                (throw (ex-info "Odd number of binding forms in let."
+                         {:bindings binding}))
+
+                :else
+                (let [[var val & rst] binding
+                      valf (crisp-compile* val)
+                      nextf (crisp-compile*
+                              (cons 'let (cons rst body)))]
+                  (fn [env]
+                    (nextf (assoc env var (valf env)))))))
+        fn (let [[arg-list & body] args
+                 bodyf (crisp-compile* (cons 'do body))]
+             (if (empty? arg-list)
+               (fn [env]
+                 (fn []
+                   (bodyf env)))
+               (let [nextf (crisp-compile*
+                             (cons 'fn (cons (rest arg-list)
+                                         body)))
+                     argname (first arg-list)]
+                 (fn [env]
+                   (fn [arg1 & args]
+                     (apply
+                       (nextf (assoc env argname arg1))
+                       args))))))
+        (let [[ff & argfs] (map crisp-compile* expr)]
+          (fn [env]
+            (let [f (ff env)]
+              (cond
+                (ifn? f)
+                (apply f (map #(% env) argfs))
+
+                (crisp-fn? f)
+                (crisp-eval
+                  (merge (.-env f) (zipmap (.-arg-list f) (map #(% env) argfs)))
+                  (cons 'do (.-body f)))
+
+                :else
+                (throw (ex-info "I don't know how to call this as a function."
+                         {:expression expr
+                          :evaled-value f}))))))))
+
+    :else
+    (throw (ex-info "I don't know how to evaluate this expression."
+             {:expression expr}))))
+
+
+(defmacro crisp-compile [expr]
+  `(sut/crisp-compile* '~expr))
--- test/domain_specific_languages_clojure/crisp_test.clj
+++ test/domain_specific_languages_clojure/crisp_test.clj
@@ -5,29 +5,62 @@
 (deftest self-evaling
   (is (= "hello" (sut/crisp-eval {} "hello")))
   (is (= 123 (sut/crisp-eval {} 123)))
-  (is (= nil (sut/crisp-eval {} nil))))
+  (is (= nil (sut/crisp-eval {} nil)))
+
+  (is (= "hello" ((sut/crisp-compile "hello") {})))
+  (is (= 123 ((sut/crisp-compile 123) {})))
+  (is (= nil ((sut/crisp-compile nil) {}))))
 
 (deftest symbol-eval-lookup
   (is (= 1 (sut/crisp-eval {'x 1} 'x)))
-  (is (thrown? clojure.lang.ExceptionInfo (sut/crisp-eval {} 'x))))
+  (is (thrown? clojure.lang.ExceptionInfo (sut/crisp-eval {} 'x)))
+
+  (is (= 1 ((sut/crisp-compile x) {'x 1})))
+  (is (thrown? clojure.lang.ExceptionInfo
+        ((sut/crisp-compile x) {}))))
 
 (deftest special-forms
   (is (= 1 (sut/crisp-eval {} '(if true 1 2))))
   (is (= 2 (sut/crisp-eval {} '(if false 1 2))))
 
+  (is (= 1 ((sut/crisp-compile (if true 1 2)) {})))
+  (is (= 2 ((sut/crisp-compile (if false 1 2)) {})))
+
+  
   (is (= 'x (sut/crisp-eval {} '(quote x))))
   (is (= 1 (sut/crisp-eval {} '(do 5 4 3 2 1))))
+
+  (is (= 'x ((sut/crisp-compile (quote x)) {})))
+  (is (= 1 ((sut/crisp-compile (do 5 4 3 2 1)) {})))
+
+
   
   (is (= 1 (sut/crisp-eval {} '(let [x 1] x))))
   (is (= 2 (sut/crisp-eval {} '(let [x 1 y 2] y))))
-  (is (thrown? clojure.lang.ExceptionInfo (sut/crisp-eval {} '(let [x] y)))))
+  (is (thrown? clojure.lang.ExceptionInfo (sut/crisp-eval {} '(let [x] y))))
+
+  (is (= 1 ((sut/crisp-compile (let [x 1] x)) {})))
+  (is (= 2 ((sut/crisp-compile (let [x 1 y 2] y)) {})))
+  (is (thrown? clojure.lang.ExceptionInfo
+        ((sut/crisp-compile (let [x] y)) {}))))
 
 (deftest function-call
   (is (= + (sut/crisp-eval {'+ +} '+)))
   (is (= 10 (sut/crisp-eval {'+ +} '(+ 4 6))))
   (is (= + (sut/crisp-eval {} +)))
+
+
   (is (sut/crisp-eval {} '(fn [] 1)))
   (is (sut/crisp-fn? (sut/crisp-eval {} '(fn [] 1))))
+
+  (is (sut/crisp-compile (fn [] 1)))
+  (is (fn? ((sut/crisp-compile (fn [] 1)) {})))
+
+  (let [f ((sut/crisp-compile (fn [a b c] c)) {})]
+    (is (= 6 (f 4 5 6))))
+
+
+  
   (is (= 1 (sut/crisp-eval {} '((fn [] 1)))))
   (is (= 2 (sut/crisp-eval {} '((fn [] 2)))))
   ;; environment
@@ -39,6 +72,20 @@
                   2)))))
   (is (not (sut/crisp-fn? 1)))
 
+  (is (= 1 ((sut/crisp-compile ((fn [] 1)))
+            {})))
+  (is (= 2 ((sut/crisp-compile ((fn [] 2)))
+            {})))
+  ;; environment
+  (is (= 10 ((sut/crisp-compile
+               (let [x 5]
+                 ((fn [y]
+                    (let [z 3]
+                      (+ x y z)))
+                  2)))
+             {'+ +})))
+
+  
 
   (is (thrown? clojure.lang.ExceptionInfo (sut/crisp-eval {} '(1))))
 
@@ -48,4 +95,18 @@
                 (f 2)))))
 
   (is (= 1 (sut/crisp-eval {'x 1} '(let [y x] y))))
+
+  (is (= 1 ((sut/crisp-compile (let [y x] y))
+            {'x 1})))
+
+  (is (thrown? clojure.lang.ExceptionInfo
+        ((sut/crisp-compile (1)) {})))
+
+  (is (= 7 ((sut/crisp-compile
+              (let [x 5
+                    f (fn [y] (+ x y))]
+                (f 2)))
+            {'+ +})))
+
+  
   )
