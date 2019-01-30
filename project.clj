7b8feb6e03c75f5aefe7c682e7f203dbaca00764
Eric Normand <ericwnormand@gmail.com>
2019-01-30 11:03:25 -0600

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,5 +3,12 @@
   :url "https://purelyfunctional.tv/"
   :license {:name "CC0 1.0 Universal (CC0 1.0) Public Domain Dedication"
             :url "http://creativecommons.org/publicdomain/zero/1.0/"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.apache.commons/commons-text "1.2"]])
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.apache.commons/commons-text "1.2"]]
+  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.22.0"]
+                             [io.aviso/pretty "0.1.34"]]
+                   :dependencies [[pjstadig/humane-test-output "0.8.3"]
+                                  [io.aviso/pretty "0.1.34"]
+                                  [instaparse "1.4.9"]]
+                   :injections [(require 'pjstadig.humane-test-output)
+                                (pjstadig.humane-test-output/activate!)]}})
